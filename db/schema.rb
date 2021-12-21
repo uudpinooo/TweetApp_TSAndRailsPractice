92c5a433abbba2ec03a4a7ff7c07bce2661e95ae
noname <uudpinooo@gmail.com>
2021-12-21 23:52:39 +0900

    usersのseedデータ記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_140533) do
+ActiveRecord::Schema.define(version: 2021_12_21_143716) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "text", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_12_21_140533) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tweets", "users"
