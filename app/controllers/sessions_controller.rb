a6be3811f03553f80e6eb4cbac7ac3602b868f94
noname <uudpinooo@gmail.com>
2021-12-28 03:48:48 +0900

    session管理実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: session_params[:email])
+    user = User.find_by_email(session_params[:email])
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user_id)
+    reset_session
     render status: :no_content
   end
 
