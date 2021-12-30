ab50d30ac836a5fc4d399a1ad02f120e30117a4a
noname <uudpinooo@gmail.com>
2021-12-31 01:39:51 +0900

    favoriteルーティング設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
+
+  validates :tweet_id, presence: true, numericality: { only_integer: true }
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates: tweet_id, uniqueness: { scope: :user_id }
 end
