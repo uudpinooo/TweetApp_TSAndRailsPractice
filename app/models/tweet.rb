2d0c068e88e4b3912f1397b0f962c77ecfdb616c
noname <uudpinooo@gmail.com>
2022-01-01 20:51:04 +0900

    dependent: :destroy記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   
   validates :text, presence: true
   validates :favorite_count, presence: true, numericality: { only_integer: true }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :tweets
-  has_many :favorites
+  has_many :tweets, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships
+  has_many :followings, through: :active_relationships, source: :followed, dependent: :destroy
+  has_many :followers, through: :passive_relationships, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness:  true
